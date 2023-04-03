f1673e0b6463ae0e6bdfde48f8f9cc03a5a0c418
Mathhew <suwmlee@gmail.com>
2023-04-03 16:33:44 +0800

    chore: update

--- dist/tracker-movie-info.user.js
+++ dist/tracker-movie-info.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TrackerMovieInfo
 // @namespace    https://github.com/Suwmlee/TrackerMovieInfo
-// @version      0.6.3
+// @version      0.7.0
 // @description  增强PT站显示更多影片信息
 // @author       suwmlee
 // @match        *://movie.douban.com/subject/*
@@ -9,6 +9,9 @@
 // @match        *://beyond-hd.me/library/title/*
 // @match        *://passthepopcorn.me/torrents*
 // @match        *://passthepopcorn.me/torrents.php?id*
+// @match        *://hdbits.org/film/info?id=*
+// @match        *://hdbits.org/details.php?id=*
+// @require      https://code.jquery.com/jquery-1.12.4.js
 // @grant        GM_xmlhttpRequest
 // @grant        GM_setValue
 // @grant        GM_getValue
@@ -532,6 +535,108 @@
     }
   };
 
+  // src/sites/hdbits.js
+  var hdbits_default = () => {
+    var site_url = decodeURI(location.href);
+    if (site_url.match(/^https?:\/\/hdbits.org\/details.php\?id=.*/)) {
+      try {
+        const addInfoToPage = (data) => {
+          if (data.cast.split("/").length > 8) {
+            data.cast = data.cast.split("/").slice(0, 8).join("/");
+          }
+          if (data.director.split("/").length > 8) {
+            data.director = data.director.split("/").slice(0, 8).join("/");
+          }
+          var label = "- ";
+          var status = "block";
+          var douban_collapse = false;
+          if (douban_collapse) {
+            label = "+ ";
+            status = "none";
+          }
+          $("#details > tbody > tr").eq(1).after(`
+                    <tr><td>
+                    <div id="l20201117" class="label collapsable" onclick="showHideEl(20201117)"><span class="plusminus">${label}</span>\u5173\u4E8E\u672C\u7247 (\u8C46\u74E3\u4FE1\u606F)</div>
+                    <div id="c20201117" class="hideablecontent" style="display: ${status};">
+                        <table class="contentlayout" cellspacing="0"><tbody>
+                            <tr>
+                                <td rowspan="3" width="2"><img src="${data.image}" style="max-width:250px;border:0px;" alt></td>
+                                <td colspan="2"><h1><a href="https://movie.douban.com/subject/${data.id}" target="_blank">${data.title}</a> (${data.year})</h1><h3>${data.aka}</h3></td>
+                            </tr>
+                            <tr>
+                                <td><table class="content" cellspacing="0" id="imdbinfo" style="white-space: nowrap;"><tbody>
+                                    <tr><th>\u8BC4\u5206</th><td>${data.average} (${data.votes}\u4EBA\u8BC4\u4EF7)</td></tr>
+                                    <tr><th>\u7C7B\u578B</th><td>${data.genre}</td></tr>
+                                    <tr><th>\u56FD\u5BB6/\u5730\u533A</th><td>${data.region}</td></tr>
+                                    <tr><th>\u5BFC\u6F14</th><td>${data.director.replace(/\//g, "<br>    ")}</td></tr>
+                                    <tr><th>\u8BED\u8A00</th><td>${data.language}</td></tr>
+                                    <tr><th>\u4E0A\u6620\u65E5\u671F</th><td>${data.releaseDate.replace(/\//g, "<br>    ")}</td></tr>
+                                    <tr><th>\u7247\u957F</th><td>${data.runtime}</td></tr>
+                                    <tr><th>\u6F14\u5458</th><td>${data.cast.replace(/\//g, "<br>    ")}</td></tr>
+                                </tbody></table></td>
+                                <td id="plotcell"><table class="content" cellspacing="0"><tbody>
+                                    <tr><th>\u7B80\u4ECB</th></tr><tr><td>${data.summary == "" ? "\u672C\u7247\u6682\u65E0\u7B80\u4ECB" : "\u3000\u3000" + data.summary.replace(/ 　　/g, "<br>\u3000\u3000")}</td></tr>
+                                </tbody></table></td>
+                            </tr>
+                            <tr>
+                                <td colspan="2" id="actors"></td>
+                            </tr>
+                        </tbody></table>
+                    </div>
+                    </td></tr>
+                `);
+          $('div.collapsable:contains("About this film (from IMDB)")').parent().find("img").first().css({ "width": "250px", "max-height": "660px" });
+          if (!douban_collapse) {
+            $('div.collapsable:contains("About this film (from IMDB)")').click();
+          }
+        };
+        var links = $("table.contentlayout").find('a[href^="https://www.imdb.com/title/"]');
+        if (links.length == 0) {
+          links = $(".showlinks").find('a[href^="https://www.imdb.com/title/"]');
+          if (links.length == 0) {
+            return;
+          }
+        }
+        getDoubanInfo(links[0].href, function(detail) {
+          if (detail) {
+            addInfoToPage(detail);
+          } else {
+            return;
+          }
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    if (site_url.match(/^https?:\/\/hdbits.org\/film\/info\?id=.*/)) {
+      try {
+        const addInfoToPage = (data) => {
+          const hdbtitle = $("table.contentlayout").find('a[href^="https://www.imdb.com/title/"]');
+          hdbtitle.prepend(`<a  target='_blank' href="https://movie.douban.com/subject/${data.id}">[${data.title.split(" ")[0]}] </a>`);
+          $("#plotcell")[0].innerHTML = `<table class="content" cellspacing="0"><tbody>
+                <tr><th>\u7B80\u4ECB</th></tr><tr><td>${data.summary == "" ? "\u672C\u7247\u6682\u65E0\u7B80\u4ECB" : "\u3000\u3000" + data.summary.replace(/ 　　/g, "<br>\u3000\u3000")}</td></tr>
+            </tbody></table>`;
+        };
+        var links = $("table.contentlayout").find('a[href^="https://www.imdb.com/title/"]');
+        if (links.length == 0) {
+          links = $(".showlinks").find('a[href^="https://www.imdb.com/title/"]');
+          if (links.length == 0) {
+            return;
+          }
+        }
+        getDoubanInfo(links[0].href, function(detail) {
+          if (detail) {
+            addInfoToPage(detail);
+          } else {
+            return;
+          }
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
+
   // src/sites/index.js
   var siteinit = () => {
     let host = location.hostname;
@@ -539,6 +644,8 @@
       beyondhd_default();
     } else if (host === "passthepopcorn.me") {
       passthepopcorn_default();
+    } else if (host === "hdbits.org") {
+      hdbits_default();
     }
   };
   var sites_default = siteinit;
